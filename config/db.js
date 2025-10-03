6dce9f1b9e7251f21a700e67d212949e338a0d22
RiyadKhan00 <piyas.khan.j.r.oh@email.com>
2025-10-03 14:53:15 +0600

    Fix MongoDB connection (remove bufferCommand option)

--- config/db.js
+++ config/db.js
@@ -1,5 +1,30 @@
 import mongoose from 'mongoose';
 
+// let cached = global.mongoose;
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// async function connectDB() {
+//   if (cached.conn) {
+//     return cached.conn;
+//   }
+
+//   if (!cached.promise) {
+//     const opts = {
+//       bufferCommand: false,
+//     };
+
+//     cached.promise = mongoose
+//       .connect(`${process.env.MONGODB_URI}/nextjsEcommerceProject`, opts)
+//       .then((mongoose) => mongoose);
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
 let cached = global.mongoose;
 
 if (!cached) {
@@ -12,12 +37,8 @@ async function connectDB() {
   }
 
   if (!cached.promise) {
-    const opts = {
-      bufferCommand: false,
-    };
-
     cached.promise = mongoose
-      .connect(`${process.env.MONGODB_URI}/nextjsEcommerceProject`, opts)
+      .connect(`${process.env.MONGODB_URI}/nextjsEcommerceProject`)
       .then((mongoose) => mongoose);
   }
 
